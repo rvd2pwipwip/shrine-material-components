8fc4b8b90d7923f280144b2d4e402349b65a6c77
Herve Desrosiers <hdesrosiers@stingray.com>
2020-04-07 13:20:15 -0400

    implementing recyclerview; bug with shr_product_card saying MaterialCardView is missing classes (Fix Build Path, Edit XML, Create Class)

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardViewHolder.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardViewHolder.kt
@@ -3,5 +3,6 @@ package com.google.codelabs.mdc.kotlin.shrine
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
+// This class stores the views that affect our card layout, so we can modify them later.
 class ProductCardViewHolder(itemView: View) //TODO: Find and store views from itemView
     : RecyclerView.ViewHolder(itemView)
--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -1,16 +1,46 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
+import kotlinx.android.synthetic.main.shr_product_grid_fragment.view.*
 
 class ProductGridFragment : Fragment() {
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+    // override onCreate() to call setHasOptionMenu with true after calling super()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment with the ProductGrid theme
+        val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+
+        // set the activity's Toolbar to be used as an ActionBar using setSupportActionBar.
+        // You can do this after the view has been created with the inflater
+        (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
+
+        // Set up the RecyclerView
+        // This includes setting the RecyclerView's layout manager, plus initializing and setting the RecyclerView's adapter.
+        view.recycler_view.setHasFixedSize(true)
+        view.recycler_view.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+        val adapter = ProductCardRecyclerViewAdapter(ProductEntry.initProductEntryList(resources))
+        view.recycler_view.adapter = adapter
+        val largePadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
+        val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
+        view.recycler_view.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+
+        return view;
+    }
+
+    // override onCreateOptionsMenu to inflate the contents of shr_toolbar_menu.xml into the toolbar:
+    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.shr_toolbar_menu, menu)
+        super.onCreateOptionsMenu(menu, menuInflater)
     }
 }
