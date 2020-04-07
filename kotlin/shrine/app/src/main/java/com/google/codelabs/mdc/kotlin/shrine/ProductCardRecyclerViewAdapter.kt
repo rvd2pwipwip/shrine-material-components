6ae240aa6c487c48fb6b5d237084ab92a103ce5d
Herve Desrosiers <hdesrosiers@stingray.com>
2020-04-07 14:12:45 -0400

    implementing recyclerview; added title price and image views to ProductCardViewHolder updated ProductCardRecyclerViewAdapter to bind product title price and image

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardRecyclerViewAdapter.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardRecyclerViewAdapter.kt
@@ -3,12 +3,14 @@ package com.google.codelabs.mdc.kotlin.shrine
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-
+import com.google.codelabs.mdc.kotlin.shrine.network.ImageRequester
 import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
 
 /**
  * Adapter used to show a simple grid of products.
  */
+
+// tell RecyclerView's adapter what to do with each card, using a ViewHolder
 class ProductCardRecyclerViewAdapter(private val productList: List<ProductEntry>) : RecyclerView.Adapter<ProductCardViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductCardViewHolder {
@@ -16,8 +18,14 @@ class ProductCardRecyclerViewAdapter(private val productList: List<ProductEntry>
         return ProductCardViewHolder(layoutView)
     }
 
+    // set the title, price and product image for each product view
     override fun onBindViewHolder(holder: ProductCardViewHolder, position: Int) {
-        // TODO: Put ViewHolder binding code here in MDC-102
+        if (position < productList.size) {
+            val product = productList[position]
+            holder.productTitle.text = product.title
+            holder.productPrice.text = product.price
+            ImageRequester.setImageFromUrl(holder.productImage, product.url)
+        }
     }
 
     override fun getItemCount(): Int {
--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardViewHolder.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardViewHolder.kt
@@ -1,8 +1,14 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.toolbox.NetworkImageView
 
 // This class stores the views that affect our card layout, so we can modify them later.
-class ProductCardViewHolder(itemView: View) //TODO: Find and store views from itemView
-    : RecyclerView.ViewHolder(itemView)
+class ProductCardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    var productImage: NetworkImageView = itemView.findViewById(R.id.product_image)
+    var productTitle: TextView = itemView.findViewById(R.id.product_title)
+    var productPrice: TextView = itemView.findViewById(R.id.product_price)
+}
